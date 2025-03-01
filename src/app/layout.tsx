fcc272dd586e36f50a3a154366b72346d005541d
ty-richard <tyrichard45@gmail.com>
2025-02-28 17:30:43 -0800

    chore: added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Navbar from "./components/navigationItems/navbar";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
