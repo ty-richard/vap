14094f1d150d74b10f429aee79caa7c01d6ce0ac
ty-richard <tyrichard45@gmail.com>
2025-03-24 12:40:49 -0700

    chore: update url logic

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Create a single PocketBase instance to reuse connections
-export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
+export const pb = new PocketBase("https://pb.vetalliancepartners.com/");
 
 // Helper to get typed responses
 export type TypedPocketBase = typeof pb;
